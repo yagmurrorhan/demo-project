b17b5a3a96f57e88f041278253f35492675d87cf
YagmurORHAN <yagmurrorhan@gmail.com>
2021-09-02 18:55:47 +0300

    Initial Commit

---
+++ src/main/java/com/example/helloworld/WebController.java
@@ -0,0 +1,14 @@
+package com.example.helloworld;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class WebController {
+    @RequestMapping("/hello")
+    public String sayHello(){
+        System.out.println("saying hello world Spring Boot");
+        return "hello";
+    }
+}
